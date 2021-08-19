fb935c18008e52e6a2922cea98d2943ea0ebd9a7
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2021-08-19 09:14:29 +0300

    Bring all together

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-const main = () => {
+const {parseArgs} = require('./args.js');
+const {getToken} = require('./config.js');
+const {getUserRepositories} = require('./github.js');
+const {filterRepositories} = require('./filter.js');
+const {clone} = require("./git");
+
+const main = (args) => {
+  const {intensive, targetDir} = parseArgs(args);
+
+  getUserRepositories(getToken())
+    .then(({data}) => {
+      const studentRepositories = filterRepositories(data, intensive);
+      clone(studentRepositories, targetDir);
+    })
 };
 
 module.exports = main;
